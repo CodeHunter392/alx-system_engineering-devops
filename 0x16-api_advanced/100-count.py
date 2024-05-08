6767040bcfcacf605e8f0ac3891ff667ea456c98
CodeHunter392 <omarbenamer392@gmail.com>
2024-05-08 06:41:29 -0700

    fix api advnnace project tasks not working properly

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-import requests
+"""Module for task 3"""
 
 
+def count_words(subreddit, word_list, word_count={}, after=None):
+    """Queries the Reddit API and returns the count of words in
+    word_list in the titles of all the hot posts
+    of the subreddit"""
+    import requests
 
-def count_words(subreddit, word_list):
-    """script that queries Reddit API then parses the title of all hot articles
-    Must be recursive
-    Results should be printed in descending order, by the count, not the title. Words with no matches should be skipped and not printed.
-    Results are based on the number of times a keyword appears, not titles it appears in. 'java java java' counts as 3 separate occurences of java.
-    To make life easier, 'java.' or 'java!' or 'java_' should not count as 'java'
-    Hints:
-        Ensure that you are NOT following redirects.
-        Your code will NOT pass if you are using a loop and not recursively calling the function!
-        This /can/ be done with a loop but the point is to use a recursive function. :)
-    Returns:
-        sorted count of given keywords
-               case-insensitive
-               delimited by spaces.
-                         exampleJavascript should count as javascript, but java should not).
-        If no posts match or the subreddit is invalid, print a newline.
-   """
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code != 200:
+        return None
+
+    info = sub_info.json()
+
+    hot_l = [child.get("data").get("title")
+             for child in info
+             .get("data")
+             .get("children")]
+    if not hot_l:
+        return None
+
+    word_list = list(dict.fromkeys(word_list))
+
+    if word_count == {}:
+        word_count = {word: 0 for word in word_list}
+
+    for title in hot_l:
+        split_words = title.split(' ')
+        for word in word_list:
+            for s_word in split_words:
+                if s_word.lower() == word.lower():
+                    word_count[word] += 1
+
+    if not info.get("data").get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
+        sorted_counts = sorted(word_count.items(),
+                               key=lambda kv: kv[1], reverse=True)
+        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+    else:
+        return count_words(subreddit, word_list, word_count,
+                           info.get("data").get("after"))
